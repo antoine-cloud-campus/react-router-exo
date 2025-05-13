9ee50d414c4d11d3d0d226ec2687ade375a2a6b7
Nubunduram <ceantoine@hotmail.fr>
2025-05-13 09:25:10 +0200

    feat: Activité 8 useNavigate to home page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,19 @@
 import { useParams } from 'react-router'
+import { useNavigate } from 'react-router';
 
 const Category = () => {
+
+    const  navigate  = useNavigate();
     const { category } = useParams();
+
+    const handleNavigateHome = () => {
+        navigate('/')
+    }
 
     return (
         <div>
             <h1>Catégorie : {category}</h1>
+            <button onClick={handleNavigateHome}>Retour à la page d'accueil</button>
         </div>
     )
 }
